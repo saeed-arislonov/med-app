14838ea01caf89074c6f4ed03f7b686f79595948
VIRTUSA\sarislonov <sarislonov@virtusa.com>
2018-01-20 23:50:50 -0500

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -4,70 +4,118 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'ngTouch', 'ui.bootstrap'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+    .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+        $ionicConfigProvider.views.transition('none')
+        // rest of the config    
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
+
+            .state('app.medicines', {
+                url: '/medicines',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/medicines.html',
+                        controller: 'ResultCtrl'
+                    }
+                }
+                /*,
+                               resolve: {
+                                   resolvedData: function ($http, $rootScope) {
+                                       var url = "http://medappteka.uz/api/medicine/view?id=" + $rootScope.selectedMedicines;
+                                       
+                                       var result = $http({
+                                           method: 'GET',
+                                           url: url
+                                       }).success(function (data) {
+                                           return data;
+                                       }).error(function (err) {
+                                           return err;
+                                       });
+                                       return result;
+                                   }
+                               } */
+            })
+
+            .state('app.profile', {
+                url: '/menu',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/profile.html'
+                    }
+                }
+            })
+            .state('app.mainPage', {
+                url: '/mainPage',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/mainPage.html',
+                        controller: 'MainPageCtrl'
+                    }
+                }
+            })
+            .state('app.clinics', {
+                url: '/clinics',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/clinics.html',
+                        controller: 'ClinicCtrl'
+                    }
+                }
+            })
+
+            .state('app.singleClinic', {
+                url: '/clinics/:id',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlist.html',
+                        controller: function ($stateParams, $scope, $http) {
+                            console.log($stateParams.id);
+                            $http.get("http://medappteka.uz/api/inst/view?id=" + $stateParams.id).success(function (data) {
+                                $scope.singleClinic = data;
+                                console.log($scope.singleClinic)
+                            }).error(function (err) {
+                                return err;
+                            });
+                            
+                            var d = new Date();
+                            $scope.todaysDate = d.getDate();
+                            $scope.todaysMonth = d.getMonth() + 1;
+                            
+                        }
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/mainPage');
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,148 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        // Form data for the login modal
+        $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    .controller('MainPageCtrl', function ($scope, $http, $rootScope, $state) {
+       /* var medicine_url = 'data/medicines.json'
+        $http({
+            method: 'GET',
+            url: medicine_url
+        }).success(function (data) {
+            $scope.medicines = data.data;
+            console.log($scope.medicines)
+        }).error(function (err) {
+            return err;
+        });*/
+    $http.get('data/medicines.json').success(function (data) {
+            $scope.medicines = data;
+            console.log($scope.medicines)
+        }).error(function (err) {
+            return err;
+        });
+
+     $scope.setMedicine = function(med){
+         $rootScope.selectedMedicine = med;
+         localStorage.setItem('med_id', med);
+     }
+     
+     $scope.setClinic = function(clinic){
+         $rootScope.selectedClinic = clinic;
+     }
+    
+        $scope.active = 'medicine';
+        $scope.productPlaceholder = 'Nazvaniya lekartsvo'
+        $scope.setActive = function (type) {
+            $scope.active = type;
+            if(type == 'medicine') {
+                $scope.productPlaceholder = 'Nazvaniya lekartsvo'
+            } else {
+                 $scope.productPlaceholder = 'Nazvaniya ucherejdeniya'
+            }
+        };
+        $scope.isActive = function (type) {
+            return type === $scope.active;
+        };
+    
+    $scope.submitSearch = function(){
+        if($scope.active == 'medicine') {
+            $state.go('app.medicines');
+        } else {
+            $state.go('app.clinics');
+        }
+    }
+
+    })
+
+    .controller('ResultCtrl', function ($scope, $stateParams, $ionicLoading, $rootScope, $http) {
+
+        var medicine_id;
+        
+        $http.get("http://medappteka.uz/api/medicine/view?id=" + $rootScope.selectedMedicine).success(function (data) {
+            $scope.medicines = data;
+            console.log($scope.medicines)
+        }).error(function (err) {
+            return err;
+        });
+    
+    $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Dynamic Group Header - 2',
+      content: 'Dynamic Group Body - 2'
+    }
   ];
-})
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isCustomHeaderOpen: false,
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+    
+
+    })
+    
+    .controller('ClinicCtrl', function ($scope, $state, $stateParams, $ionicLoading, $rootScope, $http) {
+
+         $http.get("http://medappteka.uz/api/inst").success(function (data) {
+            $scope.clinics = data.data;
+            console.log($scope.clinics)
+        }).error(function (err) {
+            return err;
+        });
+    
+        $scope.go = function(param){
+            $state.go('app.singleClinic', {id: param})
+        }
+
+    });
